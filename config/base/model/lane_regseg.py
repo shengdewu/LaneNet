fed46b9dd958fe4e7a2b24d406983e4065901cd0
boofish <786222104@qq.com>
2025-04-28 16:25:35 +0800

    add config

---
+++ config/base/model/lane_regseg.py
@@ -0,0 +1,65 @@
+# model settings
+num_lanes = 4
+grid_num = 100
+ignore_index = 255
+
+model = dict(
+    name='EncoderDecoder',
+    ignore_index=ignore_index,
+    encoder=dict(
+        name='RegSegEncoder',
+        stem_channels=32,
+        stages=[
+            [[48, [1], 24, 2, 4], [48, [1], 24, 1, 4]],
+            [[120, [1], 24, 2, 4], *[[120, [1], 24, 1, 4]] * 5],
+            [
+                [336, [1], 24, 2, 4],
+                [336, [1], 24, 1, 4],
+                [336, [1, 2], 24, 1, 4],
+                *[[336, [1, 4], 24, 1, 4]] * 4,
+                *[[336, [1, 14], 24, 1, 4]] * 6,
+                [384, [1, 14], 24, 1, 4],
+            ],
+        ],
+        out_indices=(0, 1, 2)
+    ),
+    decoder=dict(
+        name='LaneHead',
+        in_channels=[48, 120, 384],
+        grid_num=grid_num,
+        num_lanes=num_lanes,
+        cls_num_per_lane=56,
+        cls_channel=1024,
+        pool_channel=8,
+        spp_levels=(1, 2, 4, 6),
+        loss_cfg=dict(
+            loss=[
+                dict(name='SoftmaxFocalLoss',
+                     param=dict(gamma=2.0, lambda_weight=1.0, ignore_index=ignore_index)),
+                dict(name='SimilarityLoss', param=dict(lambda_weight=1.0), input_name=['logits']),
+                dict(name='StraightLoss', param=dict(lambda_weight=1.0), input_name=['logits']),
+            ]
+        )
+    ),
+    auxiliary=dict(
+        name='LaneHeadAux',
+        in_channels=[48, 120, 384],
+        aux_channel=128,
+        num_lanes=num_lanes,
+        loss_cfg=dict(
+            loss=[
+                dict(name='GeneralizedCELoss',
+                     param=dict(lambda_weight=1.0, apply_sigmoid=False, ignore_index=ignore_index))
+            ])
+    )
+)
+
+trainer = dict(
+    name='LaneTrainer',
+    weights='',
+    enable_epoch_method=False,
+    model=dict(
+        name='LaneModel',
+        generator=model,
+    )
+)
