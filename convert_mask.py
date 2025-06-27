eb1b7f1ad1329fea36a8bd1c0d25216470ac02c3
boofish <786222104@qq.com>
2025-06-27 08:41:08 +0800

    update

--- convert_mask.py
+++ convert_mask.py
@@ -9,7 +9,7 @@ import numpy as np
 
 def convert_mask(in_path, out_path):
     """
-    :param in_path: 使用 x-anylabel 标注的折线数据 (jpg, json)
+    :param in_path: 使用 x-anylabel 标注的折线数据 (jpg, json) 标签从 1 开始
     :param out_path:
     :return:
     """
@@ -20,23 +20,22 @@ def convert_mask(in_path, out_path):
         with open(f'{in_path}/{name}', mode='r') as f:
             anns = json.load(f)
 
-        if not os.path.exists(f'{out_path}/{name}'):
-            shutil.copy2(f'{in_path}/{name}', f'{out_path}/{name}')
-
         arr = name.split('.')
         mask_name = f'{out_path}/{arr[0]}.png'
-        if os.path.exists(mask_name):
-            continue
 
         img = cv2.imread(f'{in_path}/{arr[0]}.jpg', cv2.IMREAD_GRAYSCALE)
-
-        if not os.path.exists(f'{out_path}/{arr[0]}.jpg'):
-            shutil.copy2(f'{in_path}/{arr[0]}.jpg', f'{out_path}/{arr[0]}.jpg')
-
         mask = np.zeros_like(img)
         for shape in anns['shapes']:
             label = int(shape['label'])
-            offset = 20 if label == 1 else -20
+            """
+            皮带边缘标签 依次是 1, 2, 3, 4, ... 
+            1, 2 是一个皮带, 3, 4 是一个皮带
+            扩张的时候 往皮带内部扩张
+            """
+            if label % 2 == 0: # 是偶数 则往右边扩张
+                offset = -20
+            else: # 是奇数则往左阔边扩张
+                offset = 20
             in_pts = [[pt[0] + offset, pt[1]] for pt in shape['points']]
             in_pts = in_pts[::-1]
             pts = shape['points'] + in_pts
@@ -48,16 +47,16 @@ def convert_mask(in_path, out_path):
             # cv2.polylines(mask, [pts], isClosed=False, color=label, thickness=4, lineType=cv2.LINE_8)
             cv2.fillPoly(mask, [pts], color=label, lineType=cv2.LINE_8)
 
-        cv2.imwrite(mask_name, mask)
-
-        img = cv2.imread(f'{in_path}/{arr[0]}.jpg', cv2.IMREAD_COLOR)
-        weight = np.zeros_like(img)
-        weight[:, :, 0] = mask * 100
-        overlapping = cv2.addWeighted(img, 0.65, weight, 0.35, 0)
-        weight[:, :, 1] = weight[:, :, 0]
-        weight[:, :, 2] = weight[:, :, 0]
-        overlapping = np.where(weight > 0, overlapping, img)
-        cv2.imwrite(f'{out_path}/{arr[0]}_weight.jpg', overlapping)
+        # cv2.imwrite(mask_name, mask)
+        #
+        # img = cv2.imread(f'{in_path}/{arr[0]}.jpg', cv2.IMREAD_COLOR)
+        # weight = np.zeros_like(img)
+        # weight[:, :, 0] = mask * 100
+        # overlapping = cv2.addWeighted(img, 0.65, weight, 0.35, 0)
+        # weight[:, :, 1] = weight[:, :, 0]
+        # weight[:, :, 2] = weight[:, :, 0]
+        # overlapping = np.where(weight > 0, overlapping, img)
+        # cv2.imwrite(f'{out_path}/{arr[0]}_weight.jpg', overlapping)
 
     return
 
@@ -87,20 +86,24 @@ def create_anchor(in_path, cls_lane=56):
 
 
 if __name__ == '__main__':
-    in_root = '/home/thinkbook/workspace/datasets/pidai-src'
-    out_root = '/home/thinkbook/workspace/datasets/pidai-2'
+    in_root = '/mnt/sda/datasets/皮带跑偏数据集合/part0'
+    out_root = '/mnt/sda/datasets/皮带跑偏数据集合/part0'
     convert_mask(in_root, out_root)
     # create_anchor(in_root, 60)
 
-    names = [name for name in os.listdir(out_root) if name.endswith('png')]
+    in_root = '/mnt/sda/datasets/皮带跑偏数据集合'
+    names = list()
+    for pname in ['part0', 'part1']:
+        names.extend([f'{pname}/{name}' for name in os.listdir(f'{in_root}/{pname}') if name.endswith('png')])
+
     random.shuffle(names)
     idx = int(len(names) * 0.9)
-    with open(f'{out_root}/train_part1.txt', mode='w') as f:
+    with open(f'{in_root}/train_part1.txt', mode='w') as f:
         for name in names[: idx]:
             arr = name.split('.')
             f.write(f'{arr[0]}.jpg {arr[0]}.png\n')
 
-    with open(f'{out_root}/test_part1.txt', mode='w') as f:
+    with open(f'{in_root}/test_part1.txt', mode='w') as f:
         for name in names[idx: ]:
             arr = name.split('.')
             f.write(f'{arr[0]}.jpg {arr[0]}.png\n')
