db4756f680ffca51ec397a1c15892e128ba5cc80
boofish <786222104@qq.com>
2025-07-02 10:20:30 +0800

    增加辅助标签选择

--- lanenet/lanetrainer.py
+++ lanenet/lanetrainer.py
@@ -18,39 +18,51 @@ class LaneTrainer(BaseTrainer):
     def after_loop(self):
         self.model.disable_train()
 
-        seg_acc = [0]
-        cls_acc = [0]
+        aux_acc = [0]
+        acc = [0]
         for i, batch in enumerate(self.test_data_loader):
             result = self.model(batch)
-            cls = batch['label'].detach().cpu()
-            seg = batch['aux_label'].detach().cpu()
+            label = batch['label'].detach().cpu()
+            aux_label = batch['aux_label'].detach().cpu()
             img = batch['img'].mul(255).add_(0.5).clamp_(0, 255)
             col_sample_step = batch['col_sample_step']
             grid_num = batch['grid_num']
 
             logits = result['logits'].detach().cpu()
-            logits_seg = result['aux_logits'].detach().cpu()
-            cls_acc.append(result['acc'])
-            seg_acc.append(result['aux_acc'])
+            logits_aux = result['aux_logits'].detach().cpu()
+            acc.append(result['acc'])
+            aux_acc.append(result['aux_acc'])
 
             bs, c, h, w = img.shape
-            bs, cls_nums, anchors, nums = logits.shape
+            if logits_aux.shape[2:] == img.shape[2:]:
+                bs, cls_nums, anchors, nums = logits.shape
+                pts_logits = logits
+                pts_label = label
+                seg_logits = logits_aux
+                seg_label = aux_label
+
+            else:
+                bs, cls_nums, anchors, nums = logits_aux.shape
+                pts_logits = logits_aux
+                pts_label = aux_label
+                seg_logits = logits
+                seg_label = label
 
             # logits = torch.flip(logits, dims=[2])
-            prob = torch.softmax(logits[:, :-1, ...], dim=1)
+            prob = torch.softmax(pts_logits[:, :-1, ...], dim=1)
             idx = torch.arange(1, grid_num[0] + 1).unsqueeze(0)
             idx = idx.repeat(bs, 1)
             idx = idx.reshape(bs, -1, 1, 1)
             loc = torch.sum(prob * idx, dim=1)
-            logits = torch.argmax(logits, dim=1)
-            loc[logits == grid_num[0]] = 0
+            logits_pts = torch.argmax(pts_logits, dim=1)
+            loc[logits_pts == grid_num[0]] = 0
 
             imgs = list()
             for b in range(bs):
                 im = img[b].cpu().permute(1, 2, 0).numpy().astype(np.uint8)
                 im = np.ascontiguousarray(im)
 
-                lines = self.test_data_loader.dataset.to_lines(cls[b].numpy(), (h, w), col_sample_step[b], w)
+                lines = self.test_data_loader.dataset.to_lines(pts_label[b].numpy(), (h, w), col_sample_step[b], w)
                 for line in lines:
                     for pt in line:
                         cv2.circle(im, pt, 5, (0, 255, 0), -1)
@@ -62,31 +74,31 @@ class LaneTrainer(BaseTrainer):
 
                 imgs.append(im[:, :, ::-1])
 
-            cv2.imwrite(f'{self.output}/{i}-lane.jpg', np.concatenate(imgs, axis=1))
+            cv2.imwrite(f'{self.output}/{i}-pts.jpg', np.concatenate(imgs, axis=1))
 
-            logits_seg = torch.argmax(logits_seg, dim=1)
+            seg_logits = torch.argmax(seg_logits, dim=1)
 
-            seg = seg[:, 0, ...]
+            seg_label = seg_label[:, 0, ...]
             fake_seg = torch.zeros_like(batch['img'])
             gt_seg = torch.zeros_like(batch['img'])
-            gt_idx = torch.unique(seg).detach().cpu().tolist()
-            fake_idx = torch.unique(logits_seg).detach().cpu().tolist()
+            gt_idx = torch.unique(seg_label).detach().cpu().tolist()
+            fake_idx = torch.unique(seg_logits).detach().cpu().tolist()
             idx = set(gt_idx + fake_idx)
             for c in idx:
                 if c == 0:
                     continue
                 r, g, b = colors.colors[c]['rgb']
-                gt_seg[:, 0, ...][seg == c] = r
-                fake_seg[:, 0, ...][logits_seg == c] = r
-                gt_seg[:, 1, ...][seg == c] = g
-                fake_seg[:, 1, ...][logits_seg == c] = g
-                gt_seg[:, 2, ...][seg == c] = b
-                fake_seg[:, 2, ...][logits_seg == c] = b
+                gt_seg[:, 0, ...][seg_label == c] = r
+                fake_seg[:, 0, ...][seg_logits == c] = r
+                gt_seg[:, 1, ...][seg_label == c] = g
+                fake_seg[:, 1, ...][seg_logits == c] = g
+                gt_seg[:, 2, ...][seg_label == c] = b
+                fake_seg[:, 2, ...][seg_logits == c] = b
             img_sample = torch.cat((img, fake_seg, gt_seg), -1)
             self.save_image(img_sample, f'{self.output}/{i}-seg.jpg', False, nrow=1, normalize=False)
 
         logging.getLogger(self.default_log_name).info(
-            f'test cls acc = {sum(cls_acc) / len(cls_acc)}, seg acc = {sum(cls_acc) / len(cls_acc)}')
+            f'test acc = {sum(acc) / len(acc)}, aux acc = {sum(aux_acc) / len(aux_acc)}')
         return
 
     def iterate_after(self, epoch):
@@ -102,7 +114,7 @@ class LaneTrainer(BaseTrainer):
             self.model.enable_train()
 
             logging.getLogger(self.default_log_name).info(
-                f'trainer run step {epoch} acc = {sum(acc) / len(acc)}, seg = {sum(aux_acc) / len(aux_acc)}')
+                f'trainer run step {epoch} acc = {sum(acc) / len(acc)}, aux_acc = {sum(aux_acc) / len(aux_acc)}')
         return
 
     @torch.no_grad()
